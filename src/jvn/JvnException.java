39560ba603497ffe06ee534e54d092e0d3b63c12
Corenthin <corenthin.zozor@gmail.com>
2024-09-23 22:21:13 +0200

    Reformat project

--- src/jvn/JvnException.java
+++ src/jvn/JvnException.java
@@ -8,24 +8,24 @@
 package jvn;
 
 /**
- * Interface of a JVN Exception. 
+ * Interface of a JVN Exception.
  */
 
 public class JvnException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	String message;
-  
-	public JvnException() {
-	}
-	
-	public JvnException(String message) {
-		this.message = message;
-	}	
-  
-	public String getMessage(){
-		return message;
-	}
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    String message;
+
+    public JvnException() {
+    }
+
+    public JvnException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/jvn/JvnRemoteServer.java
+++ src/jvn/JvnRemoteServer.java
@@ -10,41 +10,41 @@
 
 package jvn;
 
-import java.rmi.*;
-import java.io.*;
+import java.io.Serializable;
+import java.rmi.Remote;
 
 
 /**
- * Remote interface of a JVN server (used by a remote JvnCoord) 
+ * Remote interface of a JVN server (used by a remote JvnCoord)
  */
 
 public interface JvnRemoteServer extends Remote {
-	    
-	/**
-	* Invalidate the Read lock of a JVN object 
-	* @param joi : the JVN object id
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-  public void jvnInvalidateReader(int joi)
-	throws java.rmi.RemoteException,jvn.JvnException;
-	    
-	/**
-	* Invalidate the Write lock of a JVN object 
-	* @param joi : the JVN object id
-	* @return the current JVN object state 
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-        public Serializable jvnInvalidateWriter(int joi)
-	throws java.rmi.RemoteException,jvn.JvnException;
-	
-	/**
-	* Reduce the Write lock of a JVN object 
-	* @param joi : the JVN object id
-	* @return the current JVN object state
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-   public Serializable jvnInvalidateWriterForReader(int joi)
-	 throws java.rmi.RemoteException,jvn.JvnException;
+
+    /**
+     * Invalidate the Read lock of a JVN object
+     *
+     * @param joi : the JVN object id
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public void jvnInvalidateReader(int joi) throws java.rmi.RemoteException, jvn.JvnException;
+
+    /**
+     * Invalidate the Write lock of a JVN object
+     *
+     * @param joi : the JVN object id
+     * @return the current JVN object state
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public Serializable jvnInvalidateWriter(int joi) throws java.rmi.RemoteException, jvn.JvnException;
+
+    /**
+     * Reduce the Write lock of a JVN object
+     *
+     * @param joi : the JVN object id
+     * @return the current JVN object state
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public Serializable jvnInvalidateWriterForReader(int joi) throws java.rmi.RemoteException, jvn.JvnException;
 
 }
 
